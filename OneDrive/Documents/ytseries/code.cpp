2c3f1c495b5fb1e513bd8403cbd20e0494eccf18
Shubham Sharma <YOUR_GITHUB_EMAIL>
2026-08-31 20:45:26 +0530

    Added C programs

---
+++ OneDrive/Documents/ytseries/code.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std ;
+
+int main()  {
+     int n = 5;
+     
+     for(int i = 1; i<=n; i++){
+      if (i%2 !=0){
+         cout <<i <<" ";
+      }
+
+     }
+
+     cout << endl;
+     return 0;
+    }    
+
+
+    
